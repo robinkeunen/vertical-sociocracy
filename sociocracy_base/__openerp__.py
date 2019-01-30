a7d06063beecd1b01622c444778f02584b081297
houssine <houssine@DESKTOP-ICALFFT>
2019-01-30 15:05:36 +0100

    [ADD] this module implements the basis of the sociocracy in Odoo

---
+++ sociocracy_base/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#
+#  Copyright (C) 2019  Coop IT Easy SCRLfs. <http://www.coopiteasy.be>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sociocracy Base",
+    "version": "1.0",
+    "depends": ["base",
+                ],
+    "author": "Coop IT Easy SCRLfs",
+    "category": "Cooperative management",
+    "website": "www.coopiteasy.be",
+    "license": "AGPL-3",
+    "description": """
+    
+    """,
+    'data': [
+#        'security/easy_my_coop_security.xml',
+#        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': True,
+}
