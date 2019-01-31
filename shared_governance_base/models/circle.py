eb57341c8c77ff63b263e3e75162f59162d74c53
houssine <houssine@DESKTOP-ICALFFT>
2019-01-31 15:31:13 +0100

    [REFACT] refact parent and child

--- shared_governance_base/models/circle.py
+++ shared_governance_base/models/circle.py
@@ -7,6 +7,7 @@ class Circle(models.Model):
 
     name = fields.Char(
         string='Name',
+        required=True,
     )
     mandate_id = fields.Many2one(
         comodel_name='shared.governance.mandate',
@@ -14,13 +15,15 @@ class Circle(models.Model):
     state = fields.Selection(
         related='mandate_id.state',
         string='State')
-    parent_circle_id = fields.Many2one(
+    parent_id = fields.Many2one(
         comodel_name='shared.governance.circle',
-        string='Parent circle')
-    childs_circle = fields.One2many(
-        'shared.governance.mandate',
-        'parent_circle_id',
-        string='Mandate')
+        string='Parent circle',
+        select=True)
+    child_ids = fields.One2many(
+        'shared.governance.circle',
+        'parent_id',
+        string='Childs Circle',
+        domain=[('state', '=', 'alive')])
     members = fields.Many2many(
         comodel_name='res.users',
         string='Members')
